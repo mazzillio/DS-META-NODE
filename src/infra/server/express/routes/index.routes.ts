004c900c67507730e3aa0b6d378a0f8298fbf411
mazzillio <mateca500@gmail.com>
2022-07-17 15:11:16 -0300

    Adicionando o middleware de erro ao projeto

--- src/infra/server/express/routes/index.routes.ts
+++ src/infra/server/express/routes/index.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 
+import { middlewareError } from "../middlewares/middlewareError";
+
 const routes = Router();
+routes.use(middlewareError);
 routes.get("/", (req, res) => res.send("Foi amigão"));
 export { routes };
